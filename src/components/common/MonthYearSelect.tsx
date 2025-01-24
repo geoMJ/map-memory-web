7ad40a63bdbef11d7d7f63a01f52643c6dce017e
Marianne Jouffroy <marianne.jouffroy@hotmail.fr>
2025-01-24 03:13:30 +0100

    set up inputs with react hook forms

--- src/components/common/MonthYearSelect.tsx
+++ src/components/common/MonthYearSelect.tsx
@@ -1,4 +1,3 @@
-import { Label } from "@/components/ui/label";
 import {
     Select,
     SelectContent,
@@ -6,71 +5,62 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { useEffect, useState } from "react";
+
 import { useTranslation } from "react-i18next";
 
 // We start with the current year (eg. 2025) and go back 100 years from there
 // TODO Maybe more years would be best
 
-const currentYear = new Date().getFullYear();
-const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
+// Also, it took me way to long to understand how to register custom components with RHF
+// ComponentProps helps pass props to nested component, in this case Select
 
-interface MonthYearSelectProps {
-    onSelectChange: (year: string, month?: string) => void
-}
+const YearSelect = ({ ...props }: React.ComponentProps<typeof Select>) => {
+    const { t } = useTranslation();
 
-const MonthYearSelect = ({ onSelectChange }:MonthYearSelectProps) => {
-
-    const {t} = useTranslation()
-    const months = t("map_page.form.fields.all_months", {returnObjects: true}) as string[]
-
-    // I tried using refs, one for each select but it wasn't very clean since I can't directly access the input element from the Select component.
-    // It had to be used with the SelectValue but it is a Span so the value was accessed via the textContent
-    // Not sure which approach is best
-    const [selectedYear, setSelectedYear] = useState("")
-    const [selectedMonth, setSelectedMonth] = useState<string|undefined>()
-
-    useEffect(() => {
-        onSelectChange(selectedYear, selectedMonth)
-    }, [selectedMonth, selectedYear])
+    const currentYear = new Date().getFullYear();
+    const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
 
     return (
-        <div className="flex space-x-2">
-            <div className="w-1/2">
-                <Label htmlFor="year">{t("map_page.form.fields.year")}</Label>
-                <Select onValueChange={setSelectedYear}>
-                    <SelectTrigger id="year">
-                        <SelectValue placeholder={t("map_page.form.fields.select_year")} />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {years.map((year) => (
-                            <SelectItem key={year} value={year.toString()}>
-                                {year}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
-            </div>
-            <div className="w-1/2">
-                <Label htmlFor="month">{t("map_page.form.fields.month")}</Label>
-                <Select onValueChange={setSelectedMonth}>
-                    <SelectTrigger id="month">
-                        <SelectValue placeholder={t("map_page.form.fields.select_month")} />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {months.map((month, index) => (
-                            <SelectItem
-                                key={index}
-                                value={month}
-                            >
-                                {month}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
-            </div>
-        </div>
+        <Select {...props}>
+            <SelectTrigger id="year">
+                <SelectValue
+                    placeholder={t("map_page.form.fields.select_year")}
+                />
+            </SelectTrigger>
+            <SelectContent>
+                {years.map((year) => (
+                    <SelectItem key={year} value={year.toString()}>
+                        {year}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     );
 };
 
-export default MonthYearSelect;
+const MonthSelect = ({ ...props }: React.ComponentProps<typeof Select>) => {
+    const { t } = useTranslation();
+
+    const months = t("map_page.form.fields.all_months", {
+        returnObjects: true,
+    }) as string[];
+
+    return (
+        <Select {...props}>
+            <SelectTrigger id="month">
+                <SelectValue
+                    placeholder={t("map_page.form.fields.select_month")}
+                />
+            </SelectTrigger>
+            <SelectContent>
+                {months.map((month, index) => (
+                    <SelectItem key={index} value={month}>
+                        {month}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+};
+
+export { YearSelect, MonthSelect };
--- src/features/map/components/MemoryForm.tsx
+++ src/features/map/components/MemoryForm.tsx
@@ -2,34 +2,102 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
-import MonthYearSelect from "@/components/common/MonthYearSelect";
+import { MonthSelect, YearSelect } from "@/components/common/MonthYearSelect";
 import { useTranslation } from "react-i18next";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+
+interface MemoryFormFields {
+    title: string;
+    description: string;
+    photo: FileList;
+    year: string;
+    month?: string | undefined;
+}
 
 const MemoryForm = () => {
+    const { t } = useTranslation();
 
-    const {t} = useTranslation()
+    // Form logic //
 
-    const logvals = (y:string, m?:string) => {
-        console.log(y+m)
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm<MemoryFormFields>();
+
+    const handleMemorySubmit: SubmitHandler<MemoryFormFields> = (data) => {
+        console.log(data);
     };
+    //////////////////////////
 
     return (
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleMemorySubmit)}>
             <div>
                 <Label htmlFor="title">{t("map_page.form.fields.title")}</Label>
-                <Input id="title" placeholder={t("map_page.form.fields.title_placeholder")} />
+                <Input
+                    id="title"
+                    placeholder={t("map_page.form.fields.title_placeholder")}
+                    {...register("title")}
+                />
             </div>
             <div>
-                <Label htmlFor="description">{t("map_page.form.fields.description")}</Label>
-                <Textarea id="description" placeholder={t("map_page.form.fields.description_placeholder")} />
+                <Label htmlFor="description">
+                    {t("map_page.form.fields.description")}
+                </Label>
+                <Textarea
+                    id="description"
+                    placeholder={t(
+                        "map_page.form.fields.description_placeholder"
+                    )}
+                    {...register("description")}
+                />
             </div>
             <div>
-                <Label htmlFor="picture">{t("map_page.form.fields.picture")}</Label>
-                <Input id="picture" type="file" accept="image/*" />
+                <Label htmlFor="picture">
+                    {t("map_page.form.fields.picture")}
+                </Label>
+                <Input
+                    id="picture"
+                    type="file"
+                    accept="image/*"
+                    {...register("photo")}
+                />
             </div>
-            <MonthYearSelect onSelectChange={logvals} />
+            <div className="flex space-x-2">
+                <div className="w-1/2">
+                    <Label htmlFor="year">
+                        {t("map_page.form.fields.year")}
+                    </Label>
+                    <Controller
+                        name="year"
+                        control={control}
+                        render={({ field }) => (
+                            <YearSelect
+                                onValueChange={field.onChange}
+                                value={field.value}
+                            />
+                        )}
+                    />
+                </div>
+                <div className="w-1/2">
+                    <Label htmlFor="month">
+                        {t("map_page.form.fields.month")}
+                    </Label>
+                    <Controller
+                        name="month"
+                        control={control}
+                        render={({ field }) => (
+                            <MonthSelect
+                                onValueChange={field.onChange}
+                                value={field.value}
+                            />
+                        )}
+                    />
+                </div>
+            </div>
             <Button type="submit" className="w-full">
-            {t("map_page.form.submit")}
+                {t("map_page.form.submit")}
             </Button>
         </form>
     );
